8fe26ad330093da1a94946c8dd03a257222ec17f
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-15 18:51:20 +0530

    [PALLAVI] Add: CheckUniqueElement

---
+++ OOP/FindUniqueElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPracticeproblems
+{
+    public class FindUniqueElement
+    {
+        public void CheckUniqueElement()
+        {
+            int[] arr1 = { 9, 24, 34, 24, 3, 15, 26, 32, 88};
+            Console.WriteLine("Unique Elements in array are: ");
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                bool flag = false;
+                for (int j = i + 1; j < arr1.Length; j++)
+                {
+                    if (arr1[i] == arr1[j])
+                    {
+                    
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                {
+                    Console.WriteLine(arr1[i]);
+                }
+            }
+        }
+    }
+    
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace OOP
+﻿using OOPPracticeproblems;
+
+namespace OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           DuplicateElement duplicateElement = new DuplicateElement();
-            duplicateElement.CheckDuplicateItem();
+            //DuplicateElement duplicateElement = new DuplicateElement();
+            // duplicateElement.CheckDuplicateItem();
+
+
+            FindUniqueElement unique = new FindUniqueElement();
+            unique.CheckUniqueElement();
+
         }
     }
 }
